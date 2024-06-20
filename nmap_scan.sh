10d3a291ffc48e8c9e250141c8e8799b941e9460
K-Scorpio <akretchakre@gmail.com>
2024-06-20 00:19:53 -0500

    Make nmap_scan.sh more compact

--- nmap_scan.sh
+++ nmap_scan.sh
@@ -1,34 +1,29 @@
 #!/bin/bash
 
+
 if [ -z "$1" ]; then
   echo "Usage: $0 <target_ip> <output_file_name>"
   exit 1
 fi
-
 
 if [ -z "$2" ]; then
   echo "Usage: $0 <target_ip> <output_file_name>"
   exit 1
 fi
 
-
 TARGET_IP="$1"
 OUTPUT_FILE_NAME="$2"
-
 
 if [ ! -d "nmap" ]; then
   mkdir nmap
 fi
-
 
 nmap_output=$(nmap $TARGET_IP -p- --min-rate=5000 -Pn --open --reason)
 
 # The output of the first scan is saved for debugging purposes
 echo "$nmap_output" > nmap_output.txt
 
-
 ports=$(echo "$nmap_output" | grep -Eo '^[0-9]+/tcp[ ]+open' | awk '{print $1}' | cut -d'/' -f1 | paste -sd "," -)
-
 
 if [ -n "$ports" ]; then
   echo "Running detailed scan on open ports: $ports"
