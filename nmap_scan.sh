a749e4714af0a1b6919d3c5c69171308d0aa8223
K-Scorpio <akretchakre@gmail.com>
2024-06-20 00:17:26 -0500

    Make nmap_scan.sh more versatile by allowing two user provided arguments

--- nmap_scan.sh
+++ nmap_scan.sh
@@ -1,19 +1,39 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+  echo "Usage: $0 <target_ip> <output_file_name>"
+  exit 1
+fi
 
-# Change the IP address accordingly
-TARGET_IP="10.10.212.189"
+
+if [ -z "$2" ]; then
+  echo "Usage: $0 <target_ip> <output_file_name>"
+  exit 1
+fi
+
+
+TARGET_IP="$1"
+OUTPUT_FILE_NAME="$2"
+
+
+if [ ! -d "nmap" ]; then
+  mkdir nmap
+fi
+
 
 nmap_output=$(nmap $TARGET_IP -p- --min-rate=5000 -Pn --open --reason)
 
 # The output of the first scan is saved for debugging purposes
 echo "$nmap_output" > nmap_output.txt
 
+
 ports=$(echo "$nmap_output" | grep -Eo '^[0-9]+/tcp[ ]+open' | awk '{print $1}' | cut -d'/' -f1 | paste -sd "," -)
+
 
 if [ -n "$ports" ]; then
   echo "Running detailed scan on open ports: $ports"
-  nmap -sC -sV -p $ports -oA nmap/CyberLens $TARGET_IP
+  nmap -sC -sV -p $ports -oA nmap/$OUTPUT_FILE_NAME $TARGET_IP
 else
   echo "No open ports found."
 fi
+
